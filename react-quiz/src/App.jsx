d5f504f03efb94834f6ae5b1816043992316fa31
Karan Saini <karansaini93100@gmail.com>
2026-01-31 16:17:39 +0530

    feat: add start screen component and styles

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./Header";
 import Main from "./Main";
+import StartScreen from "./StartScreen";
 import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
-      <Main></Main>
+      <Main>
+        <StartScreen />
+      </Main>
       <Footer></Footer>
     </div>
   );
