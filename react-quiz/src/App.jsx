2d72fe712cc6493b62e6d5db978bd083e1bcd5dd
Karan Saini <karansaini93100@gmail.com>
2026-01-31 15:12:00 +0530

    feat: add basic layout structure

---
+++ react-quiz/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  );
+}
