6cc0dc0c341aa6babc3eb1dd9c72c76e85228479
Karan Saini <karansaini93100@gmail.com>
2026-02-01 14:36:08 +0530

    feat: handle quiz loading and error states with useReducer

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,18 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Footer from "./Footer";
 
+const initialState = {
+  status: "loading",
+  questions: [],
+};
+
+function reducer(state, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: payload };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown!");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { status } = state;
+
   useEffect(() => {
     async function fetchQuestions() {
       try {
         const res = await fetch("http://localhost:8000/questions");
         const data = await res.json();
+
+        dispatch({ type: "dataReceived", payload: data });
         console.log(res);
         console.log(data);
       } catch (err) {
+        dispatch({ type: "dataFailed" });
         console.log(err);
       }
     }
@@ -23,7 +49,9 @@ export default function App() {
     <div className="app">
       <Header />
       <Main>
-        <StartScreen />
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
       </Main>
       <Footer></Footer>
     </div>
