21cb3828ed7a03e3f286b0ec4b57132ecbacd7fc
Karan Saini <karansaini93100@gmail.com>
2026-02-01 11:13:07 +0530

    feat: fetch quiz questions using useEffect

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import StartScreen from "./StartScreen";
 import Footer from "./Footer";
 
 export default function App() {
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        console.log(res);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
