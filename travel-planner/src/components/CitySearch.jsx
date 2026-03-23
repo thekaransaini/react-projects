b61b1e59574415dc61b71b3bc4aec396a0c131d2
Karan Saini <karansaini93100@gmail.com>
2026-03-23 07:57:17 +0530

    feat: enable city search functionality with API integration and env configuration

--- travel-planner/src/components/CitySearch.jsx
+++ travel-planner/src/components/CitySearch.jsx
@@ -1,8 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./CitySearch.module.css";
+import { useNavigate } from "react-router-dom";
+
+const BASE_URL = "https://api.geoapify.com/v1/geocode/search";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export default function CitySearch() {
   const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const controller = new AbortController();
+    async function fetchCities() {
+      try {
+        if (query.length <= 3) return;
+
+        const res = await fetch(`${BASE_URL}?text=${query}&apiKey=${API_KEY}`, {
+          signal: controller.signal,
+        });
+        const data = await res.json();
+
+        if (!data.features[0]?.properties.lat) return;
+
+        navigate(
+          `form?lat=${data.features[0].properties.lat}&lng=${data.features[0].properties.lon}`,
+        );
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.log(err);
+        }
+      }
+    }
+    fetchCities();
+
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
+
   return (
     <div className={styles.citySearch}>
       <input
