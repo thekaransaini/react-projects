02ec6ee5a59b47e2eb0828d9657260d885a119d4
Karan Saini <karansaini93100@gmail.com>
2026-03-27 04:49:44 +0530

    fix: leaflet marker using CDN

--- travel-planner/client/src/components/Map.jsx
+++ travel-planner/client/src/components/Map.jsx
@@ -14,14 +14,12 @@ import Polyline from "./Polyline";
 import { useNavigate } from "react-router-dom";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: null,
 });
 
 const defaultPosition = [28.7041, 77.1025];
