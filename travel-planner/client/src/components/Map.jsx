13da72eb5589fafd36068bf901884695c7b5de41
Karan Saini <karansaini93100@gmail.com>
2026-03-27 04:38:57 +0530

    fix: leaflet marker icon issue

--- travel-planner/client/src/components/Map.jsx
+++ travel-planner/client/src/components/Map.jsx
@@ -12,6 +12,17 @@ import {
 } from "react-leaflet";
 import Polyline from "./Polyline";
 import { useNavigate } from "react-router-dom";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const defaultPosition = [28.7041, 77.1025];
 
